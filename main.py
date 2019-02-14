9bbf168b38bf76d82f9d85cb5f3ff7083c2c3af5
2004Senya <arseniy-selin-2004@yandex.ru>
2019-02-14 18:54:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,33 @@ with open("newsafr.json") as newsafr_file:
       i += 1
 
 words_sort = sorted(words, key=len)
-words_sort_reverse = words_sort.reverse()
+words_sort.reverse()
 
 i = 1
 while i <= 10:
   print(str(i) + ".", words_sort[i])
   i += 1
 
+print("\n")
 # ==============================
 
-tree = ET.parse("newsafr.xml")
+parser = ET.XMLParser(encoding = 'utf-8')
+
+words_xml = []
+tree = ET.parse("newsafr.xml", parser)
 root = tree.getroot()
-print(root.tag)
+xml_items = root.findall("channel/item/description")
+for xmli in xml_items:
+  i = 0
+  while i < len(xmli.text.split(" ")):
+    if not xmli.text.split(" ")[i] in words_xml:
+      words_xml.append(xmli.text.split(" ")[i])
+    i += 1
+
+words_sort_xml = sorted(words_xml, key=len)
+words_sort_xml.reverse()
+
+i = 1
+while i <= 10:
+  print(str(i) + ".", words_sort_xml[i])
+  i += 1
